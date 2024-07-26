27c3759b217d426bb3f509e19fc586bb8ff01626
Junaid Ahmed <j.ahmed.ja7a7@gmail.com>
2024-07-27 00:53:08 +0500

    Removed Misc. Cleande dir

--- assets/js/typewriter.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const nameElement = document.getElementById("name");
-  const descriptionElement = document.getElementById("description");
-
-  nameElement.classList.add("typewriter");
-  descriptionElement.classList.add("typewriter");
-});
