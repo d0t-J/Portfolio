b2ee33c4b44374316a75156cb9920c695d77392d
Junaid Ahmed <j.ahmed.ja7a7@gmail.com>
2024-07-26 21:58:21 +0500

    Updated elements

---
+++ assets/js/typewriter.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const nameElement = document.getElementById("name");
+  const descriptionElement = document.getElementById("description");
+
+  nameElement.classList.add("typewriter");
+  descriptionElement.classList.add("typewriter");
+});
